0fe0ce85b829eb61830195c2a8eeea39bfd9f18d
Manuel Bayona <manudesarrolla07@gmail.com>
2022-08-17 13:44:50 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,69 @@
+window.sr = ScrollReveal();
+  sr.reveal('#intro', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+window.sr = ScrollReveal();
+  sr.reveal('#about-me', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+window.sr = ScrollReveal();
+  sr.reveal('#skills', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+window.sr = ScrollReveal();
+  sr.reveal('#images', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });  
+  
+window.sr = ScrollReveal();
+  sr.reveal('#images-basics', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });  
+  
+window.sr = ScrollReveal();
+  sr.reveal('#images-languages', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });  
+  
+window.sr = ScrollReveal();
+  sr.reveal('#images-DB', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });  
+
+window.sr = ScrollReveal();
+  sr.reveal('#projects', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });  
+  
+window.sr = ScrollReveal();
+  sr.reveal('#menu_lateral_der', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });  
+  
+window.sr = ScrollReveal();
+  sr.reveal('#menu_lateral_izq', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });  
